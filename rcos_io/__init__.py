f1147935459f78a78bf1e101e9b106310192d953
Frank Matranga <thefrankmatranga@gmail.com>
2022-10-21 23:31:02 -0400

    Rename package without hyphen

--- rcos_io/__init__.py
+++ rcos_io/__init__.py
@@ -1,19 +1,20 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
+from rcos_io.auth import login_required
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
     )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
@@ -25,11 +26,17 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/')
+    @app.route("/")
     def index():
-        return 'Hello, World!'
+        return render_template("index.html")
+
+    @app.route("/secret")
+    @login_required
+    def secret():
+        return "Hello logged in users!"
 
     from . import auth
+
     app.register_blueprint(auth.bp)
 
     return app
