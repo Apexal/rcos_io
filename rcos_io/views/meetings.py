446bd786ce7bfc811ccc9b9ef52e7a2e1dcbdb28
Frank Matranga <thefrankmatranga@gmail.com>
2022-10-27 23:34:47 -0400

    Add new meeting form

--- rcos_io/views/meetings.py
+++ rcos_io/views/meetings.py
@@ -11,11 +11,25 @@ eastern = timezone('US/Eastern')
 
 @bp.route("/")
 def meetings():
+    """Renders the main meetings template which shows a calendar that fetches events from the API route."""
     return render_template("meetings/meetings.html")
+
+@bp.route("/add", methods=("GET", "POST"))
+def add_meeting():
+    """Renders the add meeting form and handles form submissions."""
+    if request.method == "GET":
+        meeting_types = ["small_group", "large_group", "workshop", "bonus", "mentors", "coordinators", "other"]
+
+        return render_template("meetings/add_meeting.html", meeting_types=meeting_types)
+    else:
+        # TODO: persist to database
+        return request.form
 
 @bp.route("/api/events")
 def events_api():
+    """Returns a JSON array of event objects that Fullcalendar can understand."""
     # TODO: use this is filtering meetings
+    # TODO: filter based on logged in status and role
     start = datetime.fromisoformat(request.args.get("start")) if "start" in request.args else None
     end = datetime.fromisoformat(request.args.get("end")) if "end" in request.args else None
 
@@ -27,6 +41,7 @@ def events_api():
     return events
 
 def meeting_to_event(meeting: Dict[str, Any]) -> Dict[str, Any]:
+    """Creates a Fullcalendar event object from a meeting."""
     return {
         "id": meeting["id"],
         "title": meeting["name"],
