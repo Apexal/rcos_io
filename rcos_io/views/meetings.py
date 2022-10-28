ed586ed7e4d2f4bc7a4c2011df4e3ab260fa50ee
Frank Matranga <thefrankmatranga@gmail.com>
2022-10-27 23:18:22 -0400

    Update meetings blueprint

--- rcos_io/views/meetings.py
+++ rcos_io/views/meetings.py
@@ -5,23 +5,25 @@ from pytz import timezone
 
 from rcos_io.db import get_meetings
 
-bp = Blueprint("meetings", __name__)
+bp = Blueprint("meetings", __name__, url_prefix="/meetings")
 
 eastern = timezone('US/Eastern')
 
-@bp.route("/meetings")
-def render_projects():
+@bp.route("/")
+def meetings():
     return render_template("meetings/meetings.html")
 
 @bp.route("/api/events")
-def events():
+def events_api():
+    # TODO: use this is filtering meetings
     start = datetime.fromisoformat(request.args.get("start")) if "start" in request.args else None
     end = datetime.fromisoformat(request.args.get("end")) if "end" in request.args else None
 
-    # TODO:
     # Fetch meetings
-    meetings = get_meetings() 
+    meetings = get_meetings()
+    # Convert them to objects that Fullcalendar can understand
     events = list(map(meeting_to_event, meetings))
+
     return events
 
 def meeting_to_event(meeting: Dict[str, Any]) -> Dict[str, Any]:
