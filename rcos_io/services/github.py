7fbd163441fc61cb9bb20531c9b2d4e0847f48f9
Bill Ni <bill.8ni@gmail.com>
2022-11-10 15:03:01 -0500

    initial commit getter

--- rcos_io/services/github.py
+++ rcos_io/services/github.py
@@ -1,11 +1,12 @@
 """
 This module contains all GitHub related functionality.
 """
-from typing import TypedDict
+from typing import TypedDict, Optional
+import datetime
 import requests
 from rcos_io.services import settings
 
-GITHUB_API_ENDPOINT = "https://api.github.com"
+GITHUB_API_URL = "https://api.github.com"
 GITHUB_AUTH_URL = (
     "https://github.com/login/oauth/authorize"
     f"?client_id={settings.GITHUB_APP_CLIENT_ID}&redirect_uri={settings.GITHUB_APP_REDIRECT_URL}"
@@ -69,7 +70,7 @@ def get_user_info(access_token: str) -> User:
     See https://docs.github.com/en/rest/users/users#get-the-authenticated-user
     """
     response = requests.get(
-        f"{GITHUB_API_ENDPOINT}/user",
+        f"{GITHUB_API_URL}/user",
         headers={
             "Authorization": f"Bearer {access_token}",
             "Content-Type": "application/vnd.github+json",
@@ -111,3 +112,113 @@ class CommitInfo(TypedDict):
     commit: Commit
     author: User
     committer: User
+
+
+"""
+   html url structure: https://github.com/Apexal/rcos_io
+    api url structure: https://api.github.com/repos/Apexal/rcos_io
+
+         get branches: https://api.github.com/repos/Apexal/rcos_io/branches
+          get commits: https://api.github.com/repos/Apexal/rcos_io/commits
+  get commits by user: ?author=username or email
+    get commits since: ?since=ISO-8601 timestamp <-- starttime
+    get commits until: ?until=ISO-8601 timestamp <-- endtime
+get commits from head: ?sha=commit sha
+"""
+
+
+def gen_params(**kwargs):
+    """
+    generates params for request querystring
+    """
+    valid_keys = {"author", "since", "until", "sha"}
+    params = {"per_page": 100}  # get 100 commits per page
+    for arg in kwargs:
+        # only include valid github api parameters
+        # only include non None values
+        if arg in valid_keys and kwargs[arg] is not None:
+            params[arg] = kwargs[arg]
+    return params
+
+
+def get_commits(
+    repolink: str,
+    starttime: datetime.datetime,
+    endtime: datetime.datetime = datetime.datetime.now(),
+    user: Optional[str] = None,
+):
+    """
+    grabs all commits on all branches
+
+    takes >
+         repolink: github html url
+        starttime: datetime object for when to grab commits since
+          endtime: datetime object for when to grab commits until
+             user: username for desired user to filter commits for
+    """
+
+    # convert datetime objects to iso strings
+    starttime, endtime = starttime.isoformat(), endtime.isoformat()
+
+    # grabs "owner_username/repo_name"
+    repoid = repolink[repolink.index("github.com/") + len("github.com/") :]
+
+    if repoid[-1] == "/":  # remove trailing slash
+        repoid = repoid[:-1]
+
+    # grabs branches for repo (limited to 100 branches due to pagination)
+    # do not add branch pagination for now due to repos not having >100 branches
+    raw_branches = requests.get(
+        f"{GITHUB_API_URL}/repos/{repoid}/branches", timeout=3
+    ).json()
+    # grab commit links for branch heads
+    branch_heads = [branch["commit"]["url"] for branch in raw_branches]
+
+    all_commits = {}
+
+    for head in branch_heads:
+        # grabs "sha" from branch head url
+        head_sha = head[head.index("commits/") + len("commits/") :]
+        # grabs commits starting from branch head
+        commit_list = requests.get(
+            f"{GITHUB_API_URL}/repos/{repoid}/commits",
+            params=gen_params(
+                sha=head_sha, author=user, since=starttime, until=endtime
+            ),
+            timeout=3,
+        ).json()
+
+        # repeatedly grab commit pages while github api returns 100 commits (max)
+        while len(commit_list) == 100:
+            break_outer = False
+            for commit in commit_list:
+                # get tree sha (!= github api sha)
+                # (this will exclude commits that are repeated e.g. merge commits)
+                tree_sha = commit["commit"]["tree"]["sha"]
+                if (
+                    tree_sha in all_commits
+                ):  # found commit upstream, stop fetching commits
+                    break_outer = True
+                    break
+                # add commit to returned dictionary
+                # attach html url and timestamp as values
+                all_commits[tree_sha] = {
+                    "url": commit["html_url"],
+                    "timestamp": commit["commit"]["author"]["date"],
+                }
+            if break_outer:
+                break
+
+            # last commit on request page
+            last_sha = commit_list[-1]["sha"]
+
+            # repeat request with last commit
+            commit_list = requests.get(
+                f"{GITHUB_API_URL}/repos/{repoid}/commits",
+                params=gen_params(
+                    sha=last_sha, author=user, since=starttime, until=endtime
+                ),
+                timeout=3,
+            ).json()
+
+    return all_commits
